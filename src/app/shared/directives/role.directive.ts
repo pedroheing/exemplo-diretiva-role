4a384832816efeef04588ec19f22d010365184ba
pedro <pedroheing@gmail.com>
2022-02-24 09:59:17 -0300

    prevenção de vazamento de memória; adicionado exemplo de role adm

--- src/app/shared/directives/role.directive.ts
+++ src/app/shared/directives/role.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 import { AuthService, Roles } from 'src/app/service/auth.service';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[roles]'
@@ -8,16 +9,27 @@ export class RoleDirective implements OnInit {
 
   @Input() roles: any[];
   private isVisible: boolean;
+  private subs: Subscription;
 
   constructor(
     private templateRef: TemplateRef<any>,
     private userService: AuthService,
     private viewContainer: ViewContainerRef
-  ) {}
+  ) {
+    this.subs = new Subscription();
+  }
 
   ngOnInit() {
-    this.userService.$usuario.subscribe(
+    const sub = this.userService.$usuario.subscribe(
       u => {
+        // se adm pode tudo, ao invés de colocar 'ADM' em todos as tags só adicione essa verificação na diretiva
+        /*if (u && u.roles.includes(Roles.Adm)){
+          if (!this.isVisible) {
+            this.isVisible = true;
+            this.viewContainer.createEmbeddedView(this.templateRef);
+          }
+          return;
+        }*/
         if (u && this.roles.some(r => u.roles.includes(r))) {
           if (!this.isVisible) {
             this.isVisible = true;
@@ -29,5 +41,11 @@ export class RoleDirective implements OnInit {
         }
       } 
     );
+    this.subs.add(sub);
   }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+  
 }
